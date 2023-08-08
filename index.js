93c6f61fe1cf84dffa6adc1a62f09bdd7d979eda
Alan Cuezzo <alanferrerocuezzo@gmail.com>
2023-08-08 02:19:15 -0300

    hice la funcion para llamar a la imagen de cada artista en artistas populares

--- index.js
+++ index.js
@@ -350,6 +350,23 @@ let popArtistaLeft = document.querySelector("#popArtistaLeft");
 let popArtistaRight = document.querySelector("#popArtistaRight");
 let item = document.querySelector(".item");
 
+const listarArtistas = ()=>{
+    item.innerHTML= "";
+     songs.forEach((elemento)=>{
+       let liArtista = document.createElement('li');
+       liArtista.classList- 'cancion';
+    
+       let cardArtista = `
+       <img src="${elemento.image}" alt="${elemento.title}" />
+      `;
+    
+    liArtista.innerHTML = cardArtista;
+    item.appendChild(liArtista);
+    });   
+    };
+    
+    listarArtistas();
+
 popArtistaRight.addEventListener('click', () => {
     item.scrollLeft += 330;
 })
